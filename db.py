93dbef04d76a43c33c001394f7a9addfb3750139
Danilenkokoko <143500116+Danilenkokoko@users.noreply.github.com>
2026-04-24 07:10:47 +0700

    Update db.py

--- db.py
+++ db.py
@@ -5,11 +5,6 @@ from psycopg2.extras import Json
 
 load_dotenv(encoding='utf-8')
 
-print("DB_NAME:", os.getenv("DB_NAME"))
-print("DB_USER:", os.getenv("DB_USER"))
-print("DB_PASSWORD:", os.getenv("DB_PASSWORD"))
-print("DB_HOST:", os.getenv("DB_HOST"))
-
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 if DB_PASSWORD:
     DB_PASSWORD = DB_PASSWORD.strip()  # удаляем лишние пробелы/символы

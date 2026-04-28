27800b230f3c10d31abf24f87736ffad75a47a4d
Danilenkokoko <danila6022000@gmail.com>
2026-04-28 21:47:51 +0700

    Оптимизирую код

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,4 +5,7 @@
 
 from handlers.character_handlers import router as character_router
 
+# Если в будущем появится роутер для заклинаний, можно добавить:
+# from handlers.spell_handlers import router as spell_router
+
 __all__ = ['character_router']

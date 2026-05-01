93ddf5bf0a783cc5effa1cf6caaa0ca93dda7780
Danilenkokoko <danila6022000@gmail.com>
2026-05-01 15:33:41 +0700

    Пересматриваю логику создания персонажа

--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -14,23 +14,20 @@ from keyboards.character_keyboards import (
     continue_kb_for_spells,
     create_class_keyboard,
     create_class_equipment_keyboard,
-    create_subclass_keyboard,
     create_background_keyboard,
-    create_background_equipment_keyboard,
     create_race_keyboard,
     create_subrace_keyboard,
     create_fighting_style_keyboard,
-    create_invocations_keyboard,
     create_character_list_keyboard,
     create_delete_keyboard,
 )
 
 __all__ = [
     'main_menu', 'cancel_kb', 'skip_kb', 'continue_kb_for_spells',
-    'create_class_keyboard', 'create_class_equipment_keyboard', 'create_subclass_keyboard',
-    'create_background_keyboard', 'create_background_equipment_keyboard',
+    'create_class_keyboard', 'create_class_equipment_keyboard',
+    'create_background_keyboard',
     'create_race_keyboard', 'create_subrace_keyboard',
-    'create_fighting_style_keyboard', 'create_invocations_keyboard',
+    'create_fighting_style_keyboard',
     'create_character_list_keyboard', 'create_delete_keyboard',
     'create_category_keyboard', 'create_spell_list_keyboard', 'create_spell_detail_keyboard',
 ]

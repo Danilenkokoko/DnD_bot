d44cbe5c1b90de8b04d0a758bd8fa96d09bb526a
Danilenkokoko <danila6022000@gmail.com>
2026-05-01 15:27:51 +0700

    Пересматриваю логику создания персонажа

--- db.py
+++ db.py
@@ -22,7 +22,7 @@ DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "DND_DB")
 DB_USER = os.getenv("DB_USER", "postgres")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "123")
 
 
 _db_pool = None
